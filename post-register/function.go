271e837c2fe9d19682c6fd8fd827cee381a0709b
dirgafebrian <dirgafebrian45@gmail.com>
2023-11-03 20:30:43 +0700

    add post register

---
+++ post-register/function.go
@@ -0,0 +1,29 @@
+package cloudfunction_todo
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	todo "github.com/mytodolist1/be_p3/modul"
+)
+
+func init() {
+	functions.HTTP("MytodolistRegister", MytodolistRegister)
+}
+
+func MytodolistRegister(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://mytodolist1.github.io")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://mytodolist1.github.io")
+	fmt.Fprintf(w, todo.GCFHandlerRegister("PASETOPRIVATEKEY", "MONGOSTRING", "mytodolist", "user", r))
+}

1fafdbc6d453b2fe45e8f9098d44491c43134748
dirgafebrian <dirgafebrian45@gmail.com>
2023-11-04 22:43:15 +0700

    Add get all users

---
+++ get-AllUser/fuction.go
@@ -0,0 +1,19 @@
+package cloudfunction_todo
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	todo "github.com/mytodolist1/be_p3/modul"
+)
+
+func init() {
+	functions.HTTP("MytodolistGetUser", MytodolistGetUser)
+}
+
+func MytodolistGetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "https://mytodolist1.github.io")
+	fmt.Fprintf(w, todo.GCFHandler("MONGOSTRING", "mytodolist", "user"))
+}

775b585d2f392835959491c94dfe8ffa1df2dd3a
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-11 20:58:02 +0700

    add new function

---
+++ user/get-UserByID/fuction.go
@@ -0,0 +1,31 @@
+package cloudfunction_todo
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	todo "github.com/mytodolist1/be_p3/modul"
+)
+
+func init() {
+	functions.HTTP("MytodolistGetUserByID", MytodolistGetUserByID)
+}
+
+func MytodolistGetUserByID(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		// w.Header().Set("Access-Control-Allow-Origin", "https://mytodolist1.github.io")
+		w.Header().Set("Access-Control-Allow-Origin", "https://mytodolist.my.id")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	// w.Header().Set("Access-Control-Allow-Origin", "https://mytodolist1.github.io")
+	w.Header().Set("Access-Control-Allow-Origin", "https://mytodolist.my.id")
+	fmt.Fprintf(w, todo.GCFHandlerGetUserByID("MONGOSTRING", "mytodolist", "user", r))
+}
